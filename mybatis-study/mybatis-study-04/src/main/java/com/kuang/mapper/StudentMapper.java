cd42f6921486c36b48d2503eaff76d69ba264cc9
luqiyu <316882012@qq.com>
2021-06-09 17:40:18 +0800

    第一次提交

--- mybatis-study/mybatis-study-04/src/main/java/com/kuang/mapper/StudentMapper.java
+++ mybatis-study/mybatis-study-04/src/main/java/com/kuang/mapper/StudentMapper.java
@@ -6,7 +6,7 @@ import com.kuang.pojo.Teacher;
 import java.util.List;
 
 public interface StudentMapper {
-    //查询嵌套
+    //查询嵌套，第一次提交
     List<Student> getStudent();
     //结果嵌套
     List<Student> getStudent2();
